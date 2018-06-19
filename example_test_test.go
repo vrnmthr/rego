cbfcca2e7283e5211b9afda5d9eb6b3cddad1a51
Varun Mathur <varun.mathur@live.com>
2018-06-18 18:06:28 -0700

    Added GoDoc examples

---
+++ example_test_test.go
@@ -0,0 +1,26 @@
+package rego
+
+// Indicates how to use the testing utility provided in the package
+func ExampleRun() {
+
+	hippo := struct {
+		Name string `json:"name"`
+		Age int `json:"age"`
+		Friends []string `json:"friends"`
+	}{
+		Name: "jim",
+		Age: 123,
+		Friends: []string{"tommy", "ben"},
+	}
+
+	test := TestCase{
+		Rule: "hippo",
+		Rules: []string{
+			`hippo = {"name":"jim", "age": 123, "friends":["tom", "ben"]} {true}`,
+		},
+		Expected: hippo,
+	}
+
+	// would put a testing.t here
+	test.Run(nil, nil, nil, "example")
+}
